20569cfd6d63ad1a059c6033d706b406e30c51aa
RAMA BHARTI <ramabharti8@gmail.com>
2022-10-15 23:17:05 +0530

    Code

---
+++ MinMaxArray.java
@@ -0,0 +1,25 @@
+import java.sql.SQLOutput;
+
+public class MinMaxArray {
+    public static void main(String args[]){
+        int [] arr = {6,8,10,2,80,50};
+        FindMinAndMax(arr);
+    }
+    static void FindMinAndMax(int[] arr){
+        if(arr==null || arr.length==0);
+        int min=arr[0];
+        int max=arr[0];
+        for(int i=1; i< arr.length; i++){
+          if(arr[i]<min){
+              min=arr[i];
+          }
+          if(arr[i]>max){
+              max=arr[i];
+          }
+        }
+        System.out.println(max + " maximum");
+        System.out.println(min + " minimum");
+      }
+}
+// Time complexity= O(n).
+//Space Complexity= O(1).
